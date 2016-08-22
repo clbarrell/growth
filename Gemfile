e73bff5239c60ee8eb0dc633105efd9597e6b920
Chris Barrell <clbarrell@gmail.com>
2016-08-22 22:36:24 +1000

    Installed active_record_session store

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'groupdate'
 # graphing!
 gem 'chartkick'
-
+gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820095426) do
+ActiveRecord::Schema.define(version: 20160822123453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,16 @@ ActiveRecord::Schema.define(version: 20160820095426) do
   end
 
   add_index "rating_answers", ["question_id"], name: "index_rating_answers_on_question_id", using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "template_questions", force: :cascade do |t|
     t.string   "text"
