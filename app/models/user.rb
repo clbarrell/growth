df1e54f0a354ec57eef57c38cc905f605570f9bc
Chris Barrell <clbarrell@gmail.com>
2016-08-22 22:28:03 +1000

    received session cookie overstore error - instaling into DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
     end
     count
   end
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
