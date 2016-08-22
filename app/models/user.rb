89f64aa705806f08cc259bad4b458b3ccc13465c
Chris Barrell <clbarrell@gmail.com>
2016-08-22 22:49:51 +1000

    Finished second feature spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
     end
     count
   end
-
+ 
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
