dc38a321195654ad72b561f205787478cb8a0070
Chris Barrell <clbarrell@gmail.com>
2016-08-22 23:11:52 +1000

    fixed redirect error on google login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,14 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     sign_in_url = new_user_session_url
-    if request.referer == sign_in_url
+    if request.referer == sign_in_url || request.referer.include?("google")
       super
       goals_path
     else
       stored_location_for(resource) || request.referer || root_path
     end
   end
-
+#
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
