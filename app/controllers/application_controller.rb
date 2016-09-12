e528bdc79c1da23336f81abbe02c97c45b76ae3c
Chris Barrell <clbarrell@gmail.com>
2016-09-12 20:57:18 +1000

    Changed callback oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # resource == user
-    sign_in_url = new_user_session_url
-    google_signin_url = user_google_oauth2_omniauth_authorize_url
-    if request.referer == sign_in_url || request.referer == google_signin_url || request.referer == new_user_registration_url
-      super
-      goals_path
-    else
-      request.referer || root_path
-    end
+    # sign_in_url = new_user_session_url
+    # google_signin_url = user_google_oauth2_omniauth_authorize_url
+    # if request.referer == sign_in_url || request.referer == google_signin_url || request.referer == new_user_registration_url
+      # super
+      # goals_path
+    # else
+      # request.referer || root_path
+    # end
+    goals_path
   end
 # request.referer == http://localhost:3000/users/auth/google_oauth2 when google
 # signin url == http://localhost:3000/users/sign_in
