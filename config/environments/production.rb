f5cc0611b121ab1fbd7a61fea7b42c065c738e74
Chris Barrell <clbarrell@gmail.com>
2016-11-01 23:02:37 +1100

    fix error on action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
     :domain         => 'growth-focus.herokuapp.com',
     :authentication => :plain,
   }
+  config.action_mailer.default_url_options = { host: 'growth-focus.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
